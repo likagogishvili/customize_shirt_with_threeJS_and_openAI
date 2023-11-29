d395109c34a3d70cb2d1b910e7d78ba15386771c
likagogishvili <likagogishvili2@gmail.com>
2023-11-29 12:58:26 +0400

    removed key

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi} from 'openai';
+import { OpenAI } from "openai";
 
 dotenv.config();
 
 const router = express.Router();
 
-const config = new Configuration({
+const config = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+const openai = new OpenAI(config);
 
-const openai = new OpenAIApi(config);
+
 
 router.route('/').get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES" })
@@ -20,14 +21,14 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const response = await openai.createImage({
+    const aiResponse = await openai.images.generate({
       prompt,
       n: 1,
-      size: '1024x1024',
-      response_format: 'b64_json'
+      size: "1024x1024",
+      response_format: "b64_json",
     });
 
-    const image = response.data.data[0].b64_json;
+    const image = aiResponse.data[0].b64_json;
 
     res.status(200).json({ photo: image });
   } catch (error) {
